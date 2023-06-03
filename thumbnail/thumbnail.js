dd5a3300cc88a8ed0ac696ada78a95f36c5380e9
artillect <oreo4455@gmail.com>
2023-06-02 20:11:36 -0700

    Updated thumbnail generator

--- thumbnail/thumbnail.js
+++ thumbnail/thumbnail.js
@@ -1,77 +1,29 @@
-// Get the canvas element by id
-var canvas = document.getElementById("thumbnail");
 
-// Get the 2D drawing context
-var ctx = canvas.getContext("2d");
 
-// Set the fill style to red
-ctx.fillStyle = "red";
+// // Set the fill style to red
+// ctx.fillStyle = "red";
 
-// Draw a rectangle at (10, 10) with width 100 and height 50
-// ctx.fillRect(10, 10, 100, 50);
+// // Draw a rectangle at (10, 10) with width 100 and height 50
+// // ctx.fillRect(10, 10, 100, 50);
 
     
-async function loadDoc() {
+async function generate() {
+    // Get the canvas element by id
+    var canvas = await document.getElementById("thumbnail");
+
+    // Get the 2D drawing context
+    var ctx = await canvas.getContext("2d");
     // Clear the canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    await drawFirstCommander();
-    await drawSecondCommander();
+    await drawFirstCommander(canvas, ctx);
+    await drawSecondCommander(canvas, ctx);
+    // Reset composite operation
     ctx.globalCompositeOperation = "source-over";
-
-    var smallCaps = new FontFace("Beleren Smallcaps", "url(../assets/fonts/Beleren2016Smallcaps-Bold.ttf)");
-    
-    // Wait for the font to load
-    await smallCaps.load();
-
-    // Add the font to the document
-    document.fonts.add(smallCaps);
-    // Add gradient background to bottom
-    var grd = ctx.createLinearGradient(0, 400, 0, 720);
-    grd.addColorStop(0, "#00000000");
-    grd.addColorStop(0.16, "#00000060");
-    grd.addColorStop(0.69, "#000000");
-    ctx.fillStyle = grd;
-    ctx.fillRect(0, 400, 1280, 720);
-    // Add names of players
-    ctx.fillStyle = "white";
-    ctx.font = "90px Beleren Smallcaps";
-    ctx.textAlign = "center";
-    ctx.textBaseline = "top";
-    ctx.fillText(document.getElementById('player1').value, 318, 551);
-    ctx.fillText(document.getElementById('player2').value, 962, 551);
-    ctx.fillText("vs", 640, 551);
-
-    // Add Brawl Hub logo
-    var logo = new Image();
-    logo.src = "../assets/images/logo.png";
-    await logo.decode();
-    ctx.drawImage(logo, 1280/2 - logo.width/4, 64, logo.width/2, logo.height/2);
-
-    var bold = new FontFace("Beleren Bold", "url(../assets/fonts/Beleren2016-Bold.ttf)");
-    await bold.load();
-    document.fonts.add(bold);
-    // Add match type text
-    ctx.font = "90px Beleren Bold";
-    ctx.textAlign = "center";
-    ctx.textBaseline = "top";
-    ctx.fillText(document.getElementById('matchType').value, 640, 348);
-
-    // Add season text
-    ctx.font = "60px Beleren Bold";
-    ctx.textAlign = "center";
-    ctx.textBaseline = "top";
-    ctx.fillText(document.getElementById('season').value, 640, 450);
-
+    await drawText(canvas, ctx);
   }
 
-  async function drawFirstCommander() {
-    // Get the canvas element by id
-    var canvas = document.getElementById("thumbnail");
-
-    // Get the 2D drawing context
-    var ctx = canvas.getContext("2d");
-
+  async function drawFirstCommander(canvas, ctx) {
     // Create a separate canvas for the mask
     var maskCanvas = document.createElement("canvas");
     maskCanvas.width = canvas.width;
@@ -107,13 +59,7 @@ async function loadDoc() {
     ctx.globalCompositeOperation = "source-over";
 }
 
-async function drawSecondCommander() {
-    // Get the canvas element by id
-    var canvas = document.getElementById("thumbnail");
-
-    // Get the 2D drawing context
-    var ctx = canvas.getContext("2d");
-
+async function drawSecondCommander(canvas, ctx) {
     // Use fetch to make an HTTP request
     card2 = encodeURIComponent(document.getElementById('card2').value);
     let response = await fetch("https://api.scryfall.com/cards/named?fuzzy=" + card2);
@@ -132,3 +78,57 @@ async function drawSecondCommander() {
         ctx.restore();
     };
 }
+
+async function drawText(canvas, ctx) {
+    var smallCaps = new FontFace("Beleren Smallcaps", "url(../assets/fonts/Beleren2016Smallcaps-Bold.ttf)");
+    
+    // Wait for the font to load
+    await smallCaps.load();
+
+    // Add the font to the document
+    document.fonts.add(smallCaps);
+    // Add gradient background to bottom
+    var grd = ctx.createLinearGradient(0, 400, 0, 720);
+    grd.addColorStop(0, "#00000000");
+    grd.addColorStop(0.16, "#00000060");
+    grd.addColorStop(0.69, "#000000");
+    ctx.fillStyle = grd;
+    ctx.fillRect(0, 400, 1280, 720);
+    // Add names of players
+    ctx.fillStyle = "white";
+    ctx.font = "90px Beleren Smallcaps";
+    ctx.textAlign = "center";
+    ctx.textBaseline = "top";
+    ctx.fillText(document.getElementById('player1').value, 318, 551);
+    ctx.fillText(document.getElementById('player2').value, 962, 551);
+    ctx.fillText("vs", 640, 551);
+
+    // Add Brawl Hub logo
+    var logo = new Image();
+    logo.src = "../assets/images/logo.png";
+    await logo.decode();
+    ctx.drawImage(logo, 1280/2 - logo.width/4, 64, logo.width/2, logo.height/2);
+
+    var bold = new FontFace("Beleren Bold", "url(../assets/fonts/Beleren2016-Bold.ttf)");
+    await bold.load();
+    document.fonts.add(bold);
+    // Add match type text with drop shadow
+    ctx.font = "90px Beleren Bold";
+    ctx.textAlign = "center";
+    ctx.textBaseline = "top";
+    ctx.fillStyle = "black";
+    ctx.fillText(document.getElementById('matchType').value, 640, 348 + 4);
+    ctx.fillStyle = "white";
+    ctx.fillText(document.getElementById('matchType').value, 640, 348);
+
+
+    // Add season text with drop shadow
+    ctx.font = "60px Beleren Bold";
+    ctx.textAlign = "center";
+    ctx.textBaseline = "top";
+    ctx.fillStyle = "black";
+    ctx.fillText(document.getElementById('season').value, 640, 450 + 4);
+    ctx.fillStyle = "white";
+    ctx.fillText(document.getElementById('season').value, 640, 450);
+}
+
